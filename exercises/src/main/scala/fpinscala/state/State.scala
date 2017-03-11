de5c419b4fe156346e72303a32f3b24d4a6ed435
Daniel Brauer <danielbrauer@meetup.com>
2017-03-11 14:32:21 -0500

    more chap 6

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -51,6 +51,11 @@ object RNG {
     (i / (Int.MaxValue.toDouble + 1), r) // got from the book
   }
 
+  // ex 6.5
+  def double2(rng: RNG): Rand[Double] = {
+    map(nonNegativeInt)(i => i / (Int.MaxValue.toDouble + 1))
+  }
+
   def intDouble(rng: RNG): ((Int,Double), RNG) = {
     val (i, r2) = rng.nextInt
     // ((i, i.toDouble), r2)
@@ -87,9 +92,50 @@ object RNG {
   }
 
 
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  // ex 6.6
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+    rng => {
+      val (a, rng2) = ra(rng)
+      val (b, rng3) = rb(rng2)
+      (f(a,b), rng3)
+    }
+  }
 
-  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = {
+
+    // book's answer... this strikes me as too complicated, difficult to comprehend,
+    // the sort of code I don't like writing/reading ...  something more verbose
+    // but less "smart" would be more maintainable and workable in a work environment...
+    fs.foldRight(unit(List[A]()))((f, acc) => map2(f, acc)(_ :: _))
+
+    // my answer...
+    // something like this...  maybe....
+//    @tailrec
+//    def loop( remainingFs: List[Rand[A]], prevR: RNG, accum : List[A] ) : Rand[List[A]] = remainingFs match {
+//      case Nil => rng => (accum, rng) // this is wrong
+//      case head :: tail =>
+//        val (a, nextR) = head(prevR)
+//        loop( tail, nextR, a :: accum)
+//    }
+//
+//    loop(fs, RNG, List[A]())
+
+
+        // if this were a list of 3 ...
+//    r1 => {
+//      val (a, r2) = fs(0)(r1)
+//      val (b, r3) = fs(1)(r2)
+//      val (c, r4) = fs(2)(r3)
+//      (List(a,b,c), r4)
+//    }
+
+//    r1 => {
+//      fs.foldLeft((List[A](), r1))( (b, r) => ( ) ) ... not sure how to do with folding...
+//    }
+
+
+  //}
 
   def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
 }
